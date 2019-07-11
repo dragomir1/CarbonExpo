4934df5a5d6416084e0c13257adf019630ba6e25
Mike Odom <5105729+ThatOdieGuy@users.noreply.github.com>
2019-07-10 20:23:01 -0700

    Adding firebase config

---
+++ configuration/firebase.js
@@ -0,0 +1,11 @@
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBx3GdS9-iOFSrH_8y0-soQAnPnUee524s",
+  authDomain: "carbon-855b7.firebaseapp.com",
+  databaseURL: "https://carbon-855b7.firebaseio.com",
+  storageBucket: "carbon-855b7.appspot.com"
+};
+
+firebase.initializeApp(firebaseConfig);
