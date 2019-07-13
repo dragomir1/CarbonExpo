2235056b8fc467075d22e675fa5f0c8cbcabc346
Mike Odom <5105729+ThatOdieGuy@users.noreply.github.com>
2019-07-13 06:55:58 -0700

    Deleting unused MainTabNavigator and AppNavigator.web

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {createAppContainer, createStackNavigator, createSwitchNavigator} from 'react-navigation';
 
-import MainTabNavigator from './MainTabNavigator';
 import HomeScreen from '../screens/HomeScreen';
 import AuthLoadingScreen from '../screens/AuthLoadingScreen';
 import LoginScreen from '../screens/LoginScreen';
